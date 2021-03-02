63abf7c00278202033647b2ebe906afafe76c469
Bruno <brmcerqueira@gmail.com>
2021-03-02 17:17:39 -0300

    Adjust

--- bagWrapper.ts
+++ bagWrapper.ts
@@ -1,11 +1,21 @@
 import { TsxProperties } from "./types.ts";
 
+let indexBag = 1;
+let indexBagWrapper = 1;
+
 export class BagWrapper {
     private _bag?: TsxProperties;
 
+    constructor() {
+        console.log("Create BagWrapper: ", indexBagWrapper++);
+    }
+
     public init() {
         if (this._bag === undefined) {
-            this._bag = {};
+            this._bag = {
+                $index: indexBag++
+            };
+            console.log("Create Bag: ", this._bag);
         } 
     }
 
