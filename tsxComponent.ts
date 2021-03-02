4de73b2e3c7c99d05e17a9252b53952530b81240
Bruno <brmcerqueira@gmail.com>
2021-03-02 16:35:49 -0300

    Adjust

--- tsxComponent.ts
+++ tsxComponent.ts
@@ -1,9 +1,9 @@
 import { TsxComplexElement } from "./elements/tsxComplexElement.ts";
 import { TsxElement, TsxProperties } from "./types.ts";
 
-export abstract class TsxComponent<T extends TsxProperties = TsxProperties> {
-    constructor(protected properties: TsxProperties, protected children?: TsxElement[], protected bag?: T) {      
+export abstract class TsxComponent<T extends TsxProperties = TsxProperties, TBag extends TsxProperties = TsxProperties> {
+    constructor(protected properties: T, protected children?: TsxElement[], protected bag?: TBag) {      
     }
 
-    public abstract render(): Promise<TsxComplexElement>
+    public abstract render(): Promise<TsxComplexElement<TBag>>
 }
