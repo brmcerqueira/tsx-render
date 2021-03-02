a998d146ec82eec157ae144cfb2aeb50acf89700
Bruno <brmcerqueira@gmail.com>
2021-03-01 22:13:50 -0300

    Adjust

--- elements/tsxFunctionElement.ts
+++ elements/tsxFunctionElement.ts
@@ -2,11 +2,11 @@ import { TsxConstructor, TsxElement, TsxFunction, TsxProperties } from "../types
 import { TsxComplexElement } from "./tsxComplexElement.ts";
 
 export class TsxFunctionElement extends TsxComplexElement {
-    constructor(private tsxFunction: TsxFunction | TsxConstructor, properties: TsxProperties, children: TsxElement[]) {
+    constructor(private tsxFunction: TsxFunction, properties: TsxProperties, children: TsxElement[]) {
         super(properties, children); 
     }
 
-    private isConstructor(func: TsxFunction | TsxConstructor): func is TsxConstructor {
+    private isConstructor(func: TsxFunction): func is TsxConstructor {
         return func.prototype && func.prototype.constructor;
     } 
 
@@ -14,10 +14,9 @@ export class TsxFunctionElement extends TsxComplexElement {
         let element: TsxComplexElement;
 
         if (this.isConstructor(this.tsxFunction)) {
-            const constructor: TsxConstructor = this.tsxFunction.prototype.constructor;
-            element = await new constructor(this.properties, this.children).render();
+            element = await new this.tsxFunction(this.properties, this.children).render();
         } else {
-            element = await this.tsxFunction(this.properties, this.children)
+            element = await this.tsxFunction(this.properties, this.children);
         }
 
         return element.render();
