c130375eadbd46c6d78f5addb0dc756283444406
Bruno Cerqueira <brmcerqueira@gmail.com>
2024-07-13 13:01:41 -0300

    Adjust

--- mod.ts
+++ mod.ts
@@ -2,6 +2,9 @@ import { React } from "./react.ts";
 
 export * from "./jsx-runtime/index.d.ts";
 export { TsxBaseElement } from "./elements/tsxBaseElement.ts";
+export { TsxComplexElement } from "./elements/tsxComplexElement.ts";
+export { TsxFragmentElement } from "./elements/tsxFragmentElement.ts";
+export { TsxPrimitiveElement } from "./elements/tsxPrimitiveElement.ts";
 export { TsxComponent } from "./tsxComponent.ts";
 export { Fragment } from "./react.ts";
 export * from "./types.ts";
